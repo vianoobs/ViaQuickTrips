9814e67de68ea3834c96385e1d5b3b5b50a4421b
MikeIke24 <mnelms48@gmail.com>
2019-02-23 09:15:00 -0600

    MN - Get access token and send in header in each request

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-const axios_1 = __importDefault(require("axios"));
+const superagent_1 = __importDefault(require("superagent"));
 const app = express();
 let accessToken;
 app.use(function (req, res, next) {
@@ -17,32 +17,30 @@ app.use(function (req, res, next) {
     }
     else {
         const authBody = {
-            client_id: ' 466c8f34-e2af-42bb-87c2-4802206168f7',
-            client_secret: 'islsiVY6tCzezyBOHkkiMEXK7Nq07mCyj3MurrvzdYU=',
-            Resource: '2cd2fb9a-18c2-4eb1-931b-8885c6151548',
-            grant_type: 'client_credentials'
+            'client_id': '466c8f34-e2af-42bb-87c2-4802206168f7',
+            'client_secret': 'islsiVY6tCzezyBOHkkiMEXK7Nq07mCyj3MurrvzdYU=',
+            'Resource': '2cd2fb9a-18c2-4eb1-931b-8885c6151548',
+            'grant_type': 'client_credentials'
         };
-        const url = 'https://login.microsoftonline.com/2a3033c2-ad76-426c-9c5a23233cde4cde/oauth2/token';
-        const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        };
-        axios_1.default.post(url, authBody, { headers: headers }).then(res => {
-            accessToken = res.data.access_token;
+        const url = 'https://login.microsoftonline.com/2a3033c2-ad76-426c-9c5a-23233cde4cde/oauth2/token';
+        superagent_1.default
+            .post(url)
+            .type("form")
+            .send(authBody).then(authRes => {
+            const body = authRes.body;
+            accessToken = body.access_token;
+            res.setHeader('access_token', accessToken);
             next();
         }).catch(err => {
-            throw err;
+            console.log(err);
         });
     }
 });
 app.get('/hello', (req, res) => {
-    console.log(accessToken);
     res.send('Hello from the project root!');
 });
 app.get('*', (req, res) => {
-    res.send({
-        env: process.env,
-        port: process.env.port
-    });
+    res.send('Nothing to see here!');
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -72,7 +70,8 @@ app.use((err, req, res) => {
     });
 });
 app.set('port', process.env.port || 8080);
-app.listen(8080, () => {
-    console.log('App started!!');
+console.log("hello");
+let server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address()['port']);
 });
 //# sourceMappingURL=index.js.map
--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,5 @@
 import express = require('express');
-import axios from 'axios';
-
+import superagent from 'superagent';
 const app = express();
 
 let accessToken;
@@ -22,18 +21,44 @@ interface IAuthRes {
     access_token: string;
 }
 
+app.use(function (req: express.Request, res: express.Response, next) {
+    console.log('Time:', Date.now());
+    // Check req obj for any needed headers
+    const token = req.header('access_token');
+    if (token && token.length > 1) {
+        accessToken = token;
+        res.setHeader('access_token', accessToken);
+        next();
+    } else {
+        const authBody: IAuthReq = {
+            'client_id': '466c8f34-e2af-42bb-87c2-4802206168f7',
+            'client_secret': 'islsiVY6tCzezyBOHkkiMEXK7Nq07mCyj3MurrvzdYU=',
+            'Resource': '2cd2fb9a-18c2-4eb1-931b-8885c6151548',
+            'grant_type': 'client_credentials'
+        };
+        const url = 'https://login.microsoftonline.com/2a3033c2-ad76-426c-9c5a-23233cde4cde/oauth2/token';
 
+        superagent
+            .post(url)
+            .type("form")
+            .send(authBody).then(authRes => {
+                const body = authRes.body as IAuthRes;
+                accessToken = body.access_token;
+                res.setHeader('access_token', accessToken);
+                next();
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+
+});
 
 app.get('/hello', (req: express.Request, res: express.Response) => {
-    console.log(accessToken);
     res.send('Hello from the project root!')
 });
 
 app.get('*', (req: express.Request, res: express.Response) => {
-    res.send({
-        env: process.env,
-        port: process.env.port
-    })
+res.send('Nothing to see here!')
 });
 
 // catch 404 and forward to error handler
