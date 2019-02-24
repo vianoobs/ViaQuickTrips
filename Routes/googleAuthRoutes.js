eac71822a9b7efece6fdb480b602d1dd888c217f
caldwell619 <caldwell8814@gmail.com>
2019-02-24 05:04:40 -0600

    changed google structure

--- Routes/googleAuthRoutes.js
+++ Routes/googleAuthRoutes.js
@@ -9,15 +9,6 @@ const googleAuthRoutes = (app) => {
     app.get("/auth/google/callback", passport.authenticate("google"), (req, res) => {
         res.send("logged in");
     });
-    // request here on root mount to see if logged in
-    app.get("/api/user", (req, res) => {
-        res.send(req.user);
-    });
-    // log users out
-    app.get("/api/logout", (req, res) => {
-        req.logout();
-        res.redirect("/");
-    });
 };
 exports.default = googleAuthRoutes;
 //# sourceMappingURL=googleAuthRoutes.js.map
--- Routes/googleAuthRoutes.ts
+++ Routes/googleAuthRoutes.ts
@@ -9,17 +9,6 @@ const googleAuthRoutes = (app) => {
     app.get("/auth/google/callback", passport.authenticate("google"), (req, res) => {
         res.send("logged in")
     });
-
-    // request here on root mount to see if logged in
-    app.get("/api/user", (req, res) => {
-        res.send(req.user)
-    });
-
-    // log users out
-    app.get("/api/logout", (req, res) => {
-        req.logout();
-        res.redirect("/")
-    });
 };
 
 export default googleAuthRoutes;
