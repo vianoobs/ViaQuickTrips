a2670cdf5da40491119d527364b4172d4de13bbb
caldwell619 <caldwell8814@gmail.com>
2019-02-28 13:45:20 -0600

    corrected build path

--- Routes/utilRoutes.js
+++ Routes/utilRoutes.js
@@ -14,12 +14,12 @@ module.exports = app => {
     if (process.env.NODE_ENV === "production"){
         // express will serve production assets ( main.js, main.css )
         // look inside client/build to serve assets
-        app.use(express.static('client/build'));
+        app.use(express.static('client/dist'));
 
         // express will serve index.html if it doesnt recognize route
         const path = require('path');
         app.get("*", (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
         })
     }
 };
