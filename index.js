292e0665cbf792a4cddc0c84d1a3c112de8552e7
caldwell619 <caldwell8814@gmail.com>
2019-02-28 13:58:51 -0600

    change

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ if (process.env.NODE_ENV === "production") {
     const path = require('path');
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname + '/client/dist/index.html'));
+
     })
 }
 
