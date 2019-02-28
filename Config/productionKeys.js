1c8afe374c0eafde2e84011dd338e4565a6754a4
caldwell619 <caldwell8814@gmail.com>
2019-02-28 13:22:28 -0600

    fixed env variables

--- Config/productionKeys.js
+++ Config/productionKeys.js
@@ -1,8 +1,8 @@
 module.exports = {
     yelpApi: process.env.yelpApi,
     googleApi: process.env.googleApi,
-    keys: process.env.cookieKeys,
-    googleClientID: process.env.googleClientID,
+    keys: process.env.cookieKey,
+    googleClientID: process.env.googleClientId,
     googleClientSecret: process.env.googleClientSecret,
     mongoURI: process.env.mongoURI,
     facebookClientToken: process.env.facebookClientToken,
